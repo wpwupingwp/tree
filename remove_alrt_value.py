07c6fdb8f3fd5b6496f980ab4db61ad0da3a64a9
Workstation <wpwupingwp@outlook.com>
2017-07-10 17:25:35 +0800

    finish remove_alrt

--- remove_alrt_value.py
+++ remove_alrt_value.py
@@ -2,8 +2,8 @@
 
 import argparse
 import os
+import re
 from timeit import default_timer as timer
-from Bio import Phylo as p
 
 
 def get_format(filename):
@@ -17,10 +17,28 @@ def get_format(filename):
             raise ValueError('Unsupport format!')
 
 
+def remove_alrt(arg):
+    if arg.alrt:
+        repl = r'\g<alrt>'
+    else:
+        repl = r'\g<bb>'
+
+    with open(arg.input, 'r') as raw:
+        old = raw.read()
+    new = re.sub(r'(?<=\))(?P<alrt>[\d\.]+)/(?P<bb>[\d\.]+)(?=:)',
+                 repl, old)
+    if arg.output is None:
+        arg.output = arg.input + '.new'
+    with open(arg.output, 'w') as output:
+        output.write(new)
+
+
 def parse_args():
-    arg = argparse.ArgumentParser(description=main.__doc__)
-    arg.add_argument('-o', '--out', default='out',
-                     help='output directory')
+    arg = argparse.ArgumentParser()
+    arg.add_argument('input', help='input file')
+    arg.add_argument('-1', '--alrt', action='store_true',
+                     help='keep SH-like alrt value')
+    arg.add_argument('-o', '--output', help='output file')
     arg.print_help()
     return arg.parse_args()
 
@@ -30,10 +48,7 @@ def main():
     """
     start = timer()
     arg = parse_args()
-    # start here
-    if not os.path.exists(arg.out):
-        os.mkdir(arg.out)
-    function()
+    remove_alrt(arg)
     end = timer()
     print('Cost {:.3f} seconds.'.format(end-start))
 
