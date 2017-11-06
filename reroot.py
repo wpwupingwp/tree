0df96350679069a8e08959e79c392c0125c7db26
Workstation <wpwupingwp@outlook.com>
2017-11-06 11:14:47 +0800

    use old root

--- reroot.py
+++ reroot.py
@@ -4,8 +4,8 @@ from sys import argv
 from ete3 import Tree
 
 old = Tree(argv[1])
-ancestor = old.get_common_ancestor('A02Cycadales_Cycadaceae',
-                                   'A02Cycadales_Zamiaceae')
+ancestor = old.get_common_ancestor('A01Ginkgoales_Ginkgoaceae',
+                                   'A02Cycadales_Cycadaceae')
 # ancestor = old.get_common_ancestor(argv[2], argv[3])
 old.set_outgroup(ancestor)
 old.write(outfile=argv[1]+'.new')
